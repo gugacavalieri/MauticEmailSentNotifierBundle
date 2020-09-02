ec72a36046fc21c7d013e5629167eeb8e683677e
Matt Breden <matt@smarterqueue.com>
2020-09-02 11:57:12 -0700

    Update to support Mautic 3

--- Config/config.php
+++ Config/config.php
@@ -15,6 +15,29 @@ return [
     'version'     => '1.0.1',
     'author'      => 'Gustavo Cavalieri',
     'services'    => array(
+        'integrations' => array(
+            'mautic.integration.emailsentnotifier' => array(
+                'class'     => '\MauticPlugin\MauticEmailSentNotifierBundle\Integration\EmailSentNotifierIntegration',
+                'arguments' => array(
+                    'event_dispatcher',
+                    'mautic.helper.cache_storage',
+                    'doctrine.orm.entity_manager',
+                    'session',
+                    'request_stack',
+                    'router',
+                    'translator',
+                    'logger',
+                    'mautic.helper.encryption',
+                    'mautic.lead.model.lead',
+                    'mautic.lead.model.company',
+                    'mautic.helper.paths',
+                    'mautic.core.model.notification',
+                    'mautic.lead.model.field',
+                    'mautic.plugin.model.integration_entity',
+                    'mautic.lead.model.dnc',
+                ),
+            ),
+        ),
         'events' => array(
             'plugin.emailsentnotifier.email.subscribe' => array(
                 'class'     => 'MauticPlugin\MauticEmailSentNotifierBundle\EventListener\CustomEmailSubscriber',
